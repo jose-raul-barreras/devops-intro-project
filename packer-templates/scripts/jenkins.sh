c4cec73ac084a2e08f0bbe6e15fffed8d0e558b4
Jose Barreras Milanes <jbarrerasmilanes2@unl.edu>
2018-03-23 09:37:37 -0500

    add ubuntu 16.04 support

--- packer-templates/scripts/jenkins.sh
+++ packer-templates/scripts/jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 # JDK and JRE are required for Jenkins
-apt-get install -y openjdk-7-jre openjdk-7-jdk unzip dos2unix
+apt-get install -y openjdk-9-jre openjdk-9-jdk unzip dos2unix
 
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
 echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list
